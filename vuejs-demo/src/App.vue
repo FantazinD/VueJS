c40a960f8b9c6441bffcf9b14cfbf07cb787a3d3
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-12-17 00:10:42 +0800

    scoped style and added script to App.vue

--- vuejs-demo/src/App.vue
+++ vuejs-demo/src/App.vue
@@ -1,3 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            name: "John Doe",
+        };
+    },
+};
+</script>
+
 <template>
-    <h1>Vue Jobs</h1>
+    <h1>{{ name }}</h1>
 </template>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
