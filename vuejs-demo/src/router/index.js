42e7e8dd0fda11fbbdf709448f08506e6223a940
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-12-28 02:42:25 +0800

    added JobView and routes

--- vuejs-demo/src/router/index.js
+++ vuejs-demo/src/router/index.js
@@ -1,5 +1,6 @@
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
+import JobView from "@/views/JobView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
@@ -8,6 +9,7 @@ const router = createRouter({
     routes: [
         { path: "/", name: "home", component: HomeView },
         { path: "/jobs", name: "jobs", component: JobsView },
+        { path: "/jobs/:id", name: "job", component: JobView },
         { path: "/:catchAll(.*)", name: "not-found", component: NotFoundView },
     ],
 });
