29b6e61f4261f64fa7e954424431b1f6b484fe95
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-12-29 05:33:46 +0800

    using reactive() instead of ref()

--- vuejs-demo/src/components/JobListings.vue
+++ vuejs-demo/src/components/JobListings.vue
@@ -1,6 +1,5 @@
 <script setup>
-import jobData from "@/jobs.json";
-import { onMounted, ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import JobListing from "./JobListing.vue";
 import axios from "axios";
 
@@ -12,14 +11,16 @@ defineProps({
     },
 });
 
-const jobs = ref([]);
+const state = reactive({ jobs: [], isLoading: true });
 
 onMounted(async () => {
     try {
         const response = await axios.get("http://localhost:5166/jobs");
-        jobs.value = response.data;
+        state.jobs = response.data;
     } catch (error) {
         console.error("Error fetching jobs.", error);
+    } finally {
+        state.isLoading = false;
     }
 });
 </script>
@@ -29,7 +30,7 @@ onMounted(async () => {
         <div class="container-xl lg:container m-auto">
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
