d7d42cae95082924b4cb04073a0b38342f707fb7
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-12-31 00:04:37 +0800

    applied proxying

--- vuejs-demo/src/components/JobListings.vue
+++ vuejs-demo/src/components/JobListings.vue
@@ -16,7 +16,7 @@ const state = reactive({ jobs: [], isLoading: true });
 
 onMounted(async () => {
     try {
-        const response = await axios.get("http://localhost:5166/jobs");
+        const response = await axios.get("/api/jobs");
         state.jobs = response.data;
     } catch (error) {
         console.error("Error fetching jobs.", error);
