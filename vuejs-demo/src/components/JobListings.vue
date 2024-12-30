635a162922a0cb4cad3e0cf5bc0dded1d4089cc3
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-12-30 13:51:11 +0800

    made JobView dynamic

--- vuejs-demo/src/components/JobListings.vue
+++ vuejs-demo/src/components/JobListings.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { onMounted, reactive, ref } from "vue";
+import { onMounted, reactive } from "vue";
 import JobListing from "./JobListing.vue";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 import axios from "axios";
 
 defineProps({
@@ -29,7 +30,13 @@ onMounted(async () => {
     <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto">
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <!-- Show loading spinner if loading is true -->
+            <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader />
+            </div>
+
+            <!-- Show LobListing if loading is false -->
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
