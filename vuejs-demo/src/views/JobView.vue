3f2409ba86e32896dc5808371c3c48c18bfe78a5
Fantazindy <paulo.pete.k.razon@gmail.com>
2025-01-07 13:16:08 +0800

    implemented deleteJob logic

--- vuejs-demo/src/views/JobView.vue
+++ vuejs-demo/src/views/JobView.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { onMounted, reactive } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 import axios from "axios";
 import BackButton from "@/components/BackButton.vue";
 
 const route = useRoute();
+const router = useRouter();
 
 const jobId = route.params.id;
 
@@ -13,6 +14,17 @@ const state = reactive({
     job: {},
     isLoading: true,
 });
+
+const deleteJob = async () => {
+    try {
+        await axios.delete(`/api/jobs/${jobId}`);
+        router.push("/jobs");
+        // toast for Job deletion success
+    } catch (error) {
+        console.error("Error deleting job", error);
+        // toast
+    }
+};
 
 onMounted(async () => {
     try {
@@ -87,6 +99,7 @@ onMounted(async () => {
                             Edit Job
                         </RouterLink>
                         <button
+                            @click="deleteJob"
                             class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                         >
                             Delete Job
